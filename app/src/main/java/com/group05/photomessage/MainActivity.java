9856d39c52c0f4b75cb7655b2fc83fe1fac26ec3
Ma Katz <matn95@gmail.com>
2015-09-19 11:59:45 +0700

    (5-2) Camera choices

--- app/src/main/java/com/group05/photomessage/MainActivity.java
+++ app/src/main/java/com/group05/photomessage/MainActivity.java
@@ -1,10 +1,18 @@
 package com.group05.photomessage;
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Locale;
 
 import android.app.ActionBar;
+import android.app.AlertDialog;
 import android.app.FragmentTransaction;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
@@ -17,6 +25,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.parse.ParseAnalytics;
 import com.parse.ParseUser;
@@ -25,6 +34,76 @@ public class MainActivity extends FragmentActivity implements
         ActionBar.TabListener {
 
     public static final String TAG = MainActivity.class.getSimpleName();
+    public static final int TAKE_PHOTO_REQUEST = 0;
+    public static final int PICK_PHOTO_REQUEST = 1;
+    public static final int MEDIA_TYPE_IMAGE = 2;
+
+    protected Uri mMediaUri;
+
+    protected DialogInterface.OnClickListener mDialogListener = new DialogInterface.OnClickListener() {
+        @Override
+        public void onClick(DialogInterface dialog, int which) {
+            switch (which) {
+                case 0: // take photo
+                    Intent takePhotoIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    mMediaUri = getOutputMediaFileUri(MEDIA_TYPE_IMAGE);
+                    if (mMediaUri == null) {
+                        // display an error
+                        Toast.makeText(MainActivity.this, R.string.error_external_storage,
+                                Toast.LENGTH_LONG).show();
+                    }
+                    else {
+                        takePhotoIntent.putExtra(MediaStore.EXTRA_OUTPUT, mMediaUri);
+                        startActivityForResult(takePhotoIntent, TAKE_PHOTO_REQUEST);
+                    }
+                    break;
+                case 1: // choose photo
+                    Intent choosePhotoIntent = new Intent(Intent.ACTION_GET_CONTENT);
+                    choosePhotoIntent.setType("image/*");
+                    startActivityForResult(choosePhotoIntent, PICK_PHOTO_REQUEST);
+                    break;
+
+            }
+        }
+
+        private Uri getOutputMediaFileUri(int mediaType) {
+            // get the URI
+
+            // 1. Get the external storage directory
+            String appName = MainActivity.this.getString(R.string.app_name);
+            File mediaStorageDir = new File(
+                    Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES),
+                    appName);
+
+            // 2. Create our subdirectory
+            if (! mediaStorageDir.exists()) {
+                if (! mediaStorageDir.mkdirs()) {
+                    Log.e(TAG, "Failed to create directory.");
+                    return null;
+                }
+            }
+
+            // 3. Create a file name
+            // 4. Create the file
+            File mediaFile;
+            Date now = new Date();
+            String timestamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.CHINA).format(now);
+
+            String path = mediaStorageDir.getPath() + File.separator;
+            if (mediaType == MEDIA_TYPE_IMAGE) {
+                mediaFile = new File(path + "IMG_" + timestamp + ".jpg");
+            }
+
+            else {
+                return null;
+            }
+
+            Log.d(TAG, "File: " + Uri.fromFile(mediaFile));
+
+            // 5. Return the file's URI
+            return Uri.fromFile(mediaFile);
+        }
+    };
 
     /**
      * The {@link android.support.v4.view.PagerAdapter} that will provide
@@ -108,14 +187,24 @@ public class MainActivity extends FragmentActivity implements
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        int itemId = item.getItemId();
+        int id = item.getItemId();
 
-        if (itemId == R.id.action_logout) {
-            ParseUser.logOut();
-            navigateToLogin();
-        } else if (itemId == R.id.action_edit_friends) {
-            Intent intent = new Intent(this, EditFriendsActivity.class);
-            startActivity(intent);
+        switch (id) {
+            case R.id.action_logout: {
+                ParseUser.logOut();
+                navigateToLogin();
+            }
+            case R.id.action_edit_friends: {
+                Intent intent = new Intent(this, EditFriendsActivity.class);
+                startActivity(intent);
+            }
+            case R.id.action_camera: {
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setItems(R.array.camera_choices, mDialogListener);
+                AlertDialog dialog = builder.create();
+                dialog.show();
+            }
+
         }
 
         return super.onOptionsItemSelected(item);
