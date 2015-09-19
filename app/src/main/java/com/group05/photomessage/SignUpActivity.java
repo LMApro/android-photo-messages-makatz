6545b539f9561e8d137e78c105c5a7a326d6c7f2
Ma Katz <matn95@gmail.com>
2015-09-19 09:56:25 +0700

    Complete Section 2

--- app/src/main/java/com/group05/photomessage/SignUpActivity.java
+++ app/src/main/java/com/group05/photomessage/SignUpActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -24,6 +25,7 @@ public class SignUpActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
         setContentView(R.layout.activity_sign_up);
         mUsername = (EditText)findViewById(R.id.usernameField);
         mPassword = (EditText)findViewById(R.id.passwordField);
@@ -50,7 +52,7 @@ public class SignUpActivity extends ActionBarActivity {
                     AlertDialog dialog = builder.create();
                     dialog.show();
                 } else {
-
+                    setProgressBarIndeterminateVisibility(true);
 
                     ParseUser user = new ParseUser();
 
@@ -61,8 +63,7 @@ public class SignUpActivity extends ActionBarActivity {
                     user.signUpInBackground(new SignUpCallback() {
                         @Override
                         public void done(ParseException e) {
-
-
+                            setProgressBarIndeterminateVisibility(false);
                             if (e == null) {
                                 Intent intent = new Intent(SignUpActivity.this, MainActivity.class);
                                 intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
