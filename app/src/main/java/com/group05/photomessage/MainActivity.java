256d45d38de650daf724a94e9a5c472b1bdde743
Ma Katz <matn95@gmail.com>
2015-09-19 18:22:51 +0700

    (7-1) Retrieving messages

--- app/src/main/java/com/group05/photomessage/MainActivity.java
+++ app/src/main/java/com/group05/photomessage/MainActivity.java
@@ -2,6 +2,7 @@ package com.group05.photomessage;
 import java.io.File;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
 import java.util.Locale;
 
 import android.app.ActionBar;
@@ -24,10 +25,16 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.parse.FindCallback;
+import com.parse.Parse;
 import com.parse.ParseAnalytics;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
 import com.parse.ParseUser;
 
 public class MainActivity extends FragmentActivity implements
